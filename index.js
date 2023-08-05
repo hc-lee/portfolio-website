e97271d6dbcb5e440dc622530f2bd2c99b945546
hc-lee <hansol_lee@yahoo.com>
2023-08-04 22:35:38 -0700

    update 8/4/23

--- index.js
+++ index.js
@@ -12,5 +12,40 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+const changeNavBackground = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            document.querySelector('.navbar').style.backgroundColor = 'transparent';
+        } else {
+            document.querySelector('.navbar').style.backgroundColor = 'white';
+        }
+    });
+});
+
+const changeNavLinkColor = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('not-intersecting'));
+        } else {
+            document.querySelectorAll('.nav-link').forEach(link => link.classList.add('not-intersecting'));
+        }
+    });
+});
+
+const changeNavBrandColor = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            document.querySelectorAll('.navbar-brand').forEach(link => link.classList.remove('not-intersecting'));
+        } else {
+            document.querySelectorAll('.navbar-brand').forEach(link => link.classList.add('not-intersecting'));
+        }
+    });
+});
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const homeSection = document.getElementById('home');
+changeNavBackground.observe(homeSection);
+changeNavLinkColor.observe(homeSection);
+changeNavBrandColor.observe(homeSection);
